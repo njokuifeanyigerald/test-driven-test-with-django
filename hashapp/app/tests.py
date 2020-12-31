b72034608bdf6c5501c0f79082329e3cfab29ede
njokuifeanyigerald <brainboyrichmond@gmail.com>
2020-12-31 17:48:35 +0100

    second commit

--- hashapp/app/tests.py
+++ hashapp/app/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from selenium import webdriver
 
-# Create your tests here.
+
+# class FunctionalTestCase(TestCase):
+#     def setUp(self):
+#         self.browser = webdriver.Chrome()
+
+#     def test_there_is_homepage(self):
+#         self.browser.get('http://localhost:8000')
+#         self.assertIn('Enter hash here:', self.browser.page_source)
+
+#     def test_hash_of_hello(self):
+#         self.browser.get('http://localhost:8000')
+#         text = self.browser.find_element_by_id('id_text')
+#         text.send_keys('hello')
+#         self.browser.find_element_by_name('submit').click()
+#         self.assertIn('2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824', self.browser.page_source)
+    
+#     def tearDown(self):
+#         self.browser.quit()
+
+
+class UnitTestCase(TestCase):
+
+    def testHomeHomepageTemplate(self):
+        response = self.client.get('/') # gives us a responses in the homepage
+
+    
+
